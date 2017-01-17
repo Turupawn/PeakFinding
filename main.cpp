78dc168a74a2135fec3b2e677f6ac092add6a19c
Turupawn <ahmed.hn.43@gmail.com>
2017-01-17 17:22:16 -0600

    primer commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Evaluador.h"
+#include <iostream>
+using namespace std;
+
+int findPeak(vector<int> numbers)
+{
+    return 0;
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
